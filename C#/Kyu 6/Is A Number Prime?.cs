089f4e177efcaebf01654d99054fd35ccbee32f5
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-10-31 17:20:26 -0400

    Update Is A Number Prime?.cs

--- C#/Kyu 6/Is A Number Prime?.cs
+++ C#/Kyu 6/Is A Number Prime?.cs
@@ -1,9 +1,12 @@
-if (n < 2) return false;
-    if (n == 2) return true;
-      
-    int maximumDividor = (int)System.Math.Sqrt(n) + 1;
-    for (int i = 2; i < maximumDividor; i++) 
-    {
-      if (n % i == 0) return false;
-    }
-    return true;
+public static bool IsPrime(int n)
+{
+   if (n < 2) return false;
+   if (n == 2) return true;
+
+   int maximumDividor = (int)Math.Sqrt(n) + 1;
+   for (int i = 2; i < maximumDividor; i++)
+   {
+        if (n % i == 0) return false;
+   }
+   return true;
+}
