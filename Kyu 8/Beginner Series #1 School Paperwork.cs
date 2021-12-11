6deeab91983fd8036cfc241387e995a91e91b247
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-12-10 20:46:41 -0500

    Create Beginner Series #1 School Paperwork.cs

---
+++ Kyu 8/Beginner Series #1 School Paperwork.cs
@@ -0,0 +1,17 @@
+  using System;
+  public static class Paper
+  {
+    public static int Paperwork(int n, int m)
+    {
+      //#Happy Coding! ^_^
+      
+      if (n < 0 || m < 0)
+      {
+        return 0;
+      }
+      else
+      {
+        return n * m;
+      }   
+    }
+  }
