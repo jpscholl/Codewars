f79af7df1be01a9489c0641640eb5f17841f78bb
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-12-15 18:10:39 -0500

    Create Volume of Cuboid.cs

---
+++ Kyu 8/Volume of Cuboid.cs
@@ -0,0 +1,8 @@
+public class Kata {
+  public static double getVolumeOfCubiod(double length, double width, double height) 
+  {
+    // Your code here...
+    return length * width * height;
+    
+  }
+}
