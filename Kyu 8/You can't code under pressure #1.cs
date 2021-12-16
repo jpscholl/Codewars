a9735294f2e99f74b23ad7a69b567b9f31971c1b
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-12-15 21:35:23 -0500

    Create You can't code under pressure #1.cs

---
+++ Kyu 8/You can't code under pressure #1.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace You_Can_t_Code_Under_Pressure_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoubleInteger(2));
+        }
+        public static int DoubleInteger(int n)
+        {
+            return n * 2;
+        }
+    }
+}
