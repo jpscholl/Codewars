daabc22cb8ab8a0e7cd4a66aeefaef1fb5fb5181
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-12-15 21:21:08 -0500

    Create Messi goals function.cs
    Really nothing to that one

---
+++ Kyu 8/Messi goals function.cs
@@ -0,0 +1,8 @@
+public class Kata
+{
+  public static int GetGoals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals)
+  {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+}
