28201a324360a2bf155da609a1e9280a4737c0f9
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-12-17 14:09:11 -0500

    Create Function 2 - Squaring a number.cs

---
+++ Kyu 8/Function 2 - Squaring a number.cs
@@ -0,0 +1,24 @@
+/*
+//  Author: Jonathan Scholl
+//  Date: 12/17/2021
+//  Project: https://www.codewars.com/kata/523b623152af8a30c6000027
+*/
+using System;
+
+namespace Function_2___squaring_an_argument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // Write a static function "Square" here which takes a double
+        // and returns the square of that double
+        public static double Square(double num)
+        {
+            return num * num;
+        }
+    }
+}
