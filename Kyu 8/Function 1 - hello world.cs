a0846fdecfecb68eeb188156375a7f4092d6afc0
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2021-12-17 14:39:38 -0500

    Create Function 1 - hello world.cs
    It said to get creative, but the creative thing I tried actually didn't work and frustrated me...

---
+++ Kyu 8/Function 1 - hello world.cs
@@ -0,0 +1,23 @@
+/*
+//  Author: Jonathan Scholl
+//  Date: 12/17/2021
+//  Project: https://www.codewars.com/kata/523b4ff7adca849afe000035
+*/
+using System;
+
+namespace Function_1___hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Greet());
+        }
+
+        public static string Greet()
+        {
+
+            return "Hello World";
+        }
+    }
+}
